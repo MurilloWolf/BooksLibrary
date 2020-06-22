58091ec9c9c9c04c42fc7825419577b31ffe4519
MurilloWolf <mwolfcavalheiro@gmail.com>
2020-06-22 00:34:17 -0300

    home page - wip

--- bookstore/src/pages/Home/index.tsx
+++ bookstore/src/pages/Home/index.tsx
@@ -1,11 +1,65 @@
 import React from "react";
 
-import { Container, Main } from "./styles";
+import {
+	Container,
+	Main,
+	Category,
+	CategoryList,
+	Aside,
+	Animation,
+	Text,
+	Search,
+	InputText,
+} from "./styles";
+import { BsSearch } from "react-icons/bs";
 import MainLayout from "../../components/layout/main";
+import Reading1 from "../../styles/assets/reading1.svg";
+import BookIcon from "../../styles/assets/book.png";
+
 const Home: React.FC = () => {
 	return (
 		<MainLayout>
-			<h1>Main</h1>
+			<Main>
+				<Aside>
+					<Text>
+						<h1>Find a book recomendation</h1>
+						<p>
+							We have a lot of books recomendation and description separate by
+							category, check de category or help us to catolog the books
+						</p>
+					</Text>
+					<CategoryList>
+						<Category>
+							<div />
+							<p>Want to Read</p>
+						</Category>
+						<Category>
+							<div />
+							<p>Reading</p>
+						</Category>
+						<Category>
+							<div />
+							<p>Read</p>
+						</Category>
+						<Category>
+							<div />
+							<p>UnCategory</p>
+						</Category>
+					</CategoryList>
+				</Aside>
+				<Animation>
+					<img src={Reading1} />
+				</Animation>
+			</Main>
+			<Search>
+				<img src={BookIcon} />
+				<div>
+					<InputText>
+						<BsSearch size={16} />
+						<input type="text" placeholder="Search a Book" />
+					</InputText>
+				</div>
+			</Search>
 		</MainLayout>
 	);
 };
--- bookstore/src/pages/Home/styles.ts
+++ bookstore/src/pages/Home/styles.ts
@@ -1,5 +1,139 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	width: 100%;
+	height: 100%;
+`;
 
-export const Main = styled.main``;
+export const Main = styled.main`
+	margin: 2rem 0;
+	padding: 2rem;
+	display: flex;
+	flex-flow: row wrap;
+	align-items: center;
+	justify-content: space-evenly;
+`;
+
+export const Text = styled.div`
+	width: auto;
+	max-width: 525px;
+	height: auto;
+`;
+
+export const CategoryList = styled.div`
+	display: flex;
+	flex-flow: column nowrap;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+`;
+
+export const Category = styled.div`
+	width: 100%;
+	max-width: 190px;
+	font-size: 20px;
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	flex-flow: row nowrap;
+	align-self: flex-start;
+
+	div {
+		border-radius: 50%;
+		height: 24px;
+		width: 24px;
+		background: #55f052;
+		margin: 1rem 0.75rem;
+	}
+	p {
+		color: #555 !important;
+	}
+
+	@media screen and (max-width: 725px) {
+		p {
+			font-size: 16px !important;
+		}
+		div {
+			height: 16px;
+			width: 16px;
+		}
+	}
+`;
+
+export const Aside = styled.aside`
+	display: flex;
+	flex-flow: column nowrap;
+	align-items: center;
+	justify-content: space-evenly;
+	margin: 2rem 0;
+	h1 {
+		font-size: 36px;
+		margin: 1rem 0;
+	}
+
+	p {
+		font-size: 24px;
+		margin: 1rem 0;
+		color: #bbb;
+	}
+
+	@media screen and (max-width: 725px) {
+		p {
+			font-size: 16px;
+		}
+	}
+`;
+
+export const Animation = styled.div`
+	height: auto;
+
+	img {
+		max-width: 625px;
+		width: 100%;
+		height: auto;
+	}
+`;
+
+export const Search = styled.div`
+	display: flex;
+	flex-flow: row wrap;
+	align-items: center;
+	justify-content: flex-start;
+	align-self: flex-start;
+	padding: 2rem;
+
+	img {
+		max-width: 138px;
+		width: auto;
+		height: auto;
+	}
+	div {
+		display: flex;
+		flex-flow: row nowrap;
+		justify-content: center;
+		align-items: center;
+	}
+
+	@media screen and (max-width: 765px) {
+		padding: 0rem 1rem;
+		align-items: center;
+		justify-content: space-evenly;
+	}
+`;
+
+export const InputText = styled.div`
+	border-bottom: 2px solid #49ed77;
+
+	width: 100%;
+	max-width: 165px;
+	input[type="text"] {
+		margin: 5px 0;
+		box-shadow: 0 0 0 0;
+		border: 0 none;
+		outline: 0;
+		padding: 5px 0;
+		font-size: 16px;
+		line-height: 1rem;
+		max-width: 120px;
+	}
+`;
